5e2e369a7303a0d4c09d3218e698a2d11d20ccc3
jinkun <1016361268@qq.com>
2022-08-19 09:53:32 +0800

    setState的异步处理

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ interface Props {}
 
 interface State {
   robotGallery: any
+  count: number
 }
 
 class App extends React.Component<Props, State> {
@@ -17,7 +18,8 @@ class App extends React.Component<Props, State> {
   constructor(props) {
     super(props)
     this.state = {
-      robotGallery: []
+      robotGallery: [],
+      count: 0
     }
   }
 
@@ -34,6 +36,25 @@ class App extends React.Component<Props, State> {
             <img src={logo} className={styles.appLogo} alt="" />
             <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
         </div>
+        <button onClick={() => {
+          // setState第二个参数是访问setState处理后的数据
+          this.setState({count: this.state.count + 1}, () => {
+            console.log('count', this.state.count);
+          })
+          // 两个 setState + 1 实现不了state + 2，原因是setState的异步处理，拿的都是上一次的state状态 + 1
+          this.setState({count: this.state.count + 1}, () => {
+            console.log('count', this.state.count);
+          })
+
+          // 解决方法，第一个参数改为函数
+          // preState preProps 都为上一个生命周期的状态
+          this.setState((preState, preProps) => {return {count: preState.count + 1}}, () => {
+            console.log('count', this.state.count);
+          })
+          // 与state的count不同步
+          console.log('count', this.state.count);
+        }}>Click</button>
+        <span>count: {this.state.count}</span>
         <ShoppingCart />
         <div className={styles.robotList}>
           {this.state.robotGallery.map(r => <Robot id={r.id} email={r.email} name={r.name} />)}
